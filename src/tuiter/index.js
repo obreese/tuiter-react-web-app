5572435b183c2205bcd330492cf427a4786a370d
obreese <breese.o@northeastern.edu>
2022-11-06 15:37:15 -0500

    fixed explore routing issue

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -38,7 +38,25 @@ function Tuiter() {
               </div>
             }
           />
-          <Route path="home" element={<ExploreComponent />} />
+          <Route
+            path="explore"
+            element={
+              <div className="row mt-2">
+                <div className="col-2 col-md-2 col-lg-1 col-xl-2">
+                  <NavigationSidebar active="explore" />
+                </div>
+                <div
+                  className="col-10 col-md-10 col-lg-7 col-xl-6"
+                  style={{ position: "relative" }}
+                >
+                  <ExploreComponent />
+                </div>
+                <div className="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
+                  <WhoToFollowList />
+                </div>
+              </div>
+            }
+          />
         </Routes>
       </Provider>
     </div>
