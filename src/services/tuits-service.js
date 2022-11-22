e50d22ee4bdf1df6a633c6e8e455044138581dd6
obreese <breese.o@northeastern.edu>
2022-11-22 14:58:07 -0500

    makes a8 forwards compatible with a9

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const API_BASE = process.env.REACT_APP_API_BASE;
-const TUITS_API = `${API_BASE}/tuits`;
+const TUITS_API = `${API_BASE}/a8/tuits`; // a8 for backwards compatibility
 
 export const createTuit = async (tuit) => {
   const response = await axios.post(TUITS_API, tuit);
