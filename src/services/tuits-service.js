2ae874f2be67e2542a29784b8d734ce42584720a
obreese <breese.o@northeastern.edu>
2022-12-04 19:58:43 -0500

    rename base url constant

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-const API_BASE = process.env.REACT_APP_API_BASE;
-const TUITS_API = `${API_BASE}/tuits`;
+const API_BASE_URL = process.env.REACT_APP_API_BASE;
+const TUITS_API = `${API_BASE_URL}/tuits`;
 
 export const createTuit = async (tuit) => {
   const response = await axios.post(TUITS_API, tuit);
